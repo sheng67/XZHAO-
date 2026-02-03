1aed781fb48c6073068e5adfada974e14d04439e
sheng67 <Shengshengs1234@gmail.com>
2026-02-03 14:37:45 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'wietv-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './player.html',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800;900&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://unpkg.com/vue@3/dist/vue.global.js'
+];
+
+// Install SW
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch resources
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
